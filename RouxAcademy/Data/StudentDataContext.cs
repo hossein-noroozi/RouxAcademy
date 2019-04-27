146c0b23902f1c138976e53754c9e0c66781c59d
hossein noroozi <hossein.noroozi0@gmail.com>
2019-04-27 11:32:11 +0430

    Sync-All

---
+++ RouxAcademy/Data/StudentDataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RouxAcademy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RouxAcademy.Data
+{
+    public class StudentDataContext : IdentityDbContext
+    {
+        public StudentDataContext(DbContextOptions<StudentDataContext> options) : base(options)
+        {
+            //Database.EnsureCreated();
+        }
+
+        public DbSet<UniGrad> uniGrads { get; set; }
+ 
+    }
+}
